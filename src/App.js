b7128e12148529dbd70632d51308d474249bc2b8
Hariharan <hariharantlr2401@gmail.com>
2024-05-21 19:26:02 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [count,setcount]=React.useState(0);
+  const [message,setmessage]=React.useState("Hiii   ");
+  
+
+  const increment=()=>{
+    setcount(count+1);    
+    // setmessage(count % 2=== 0 ? "I'm even" : "I'm odd");
+    console.log(count);
+    console.log(count % 2 === 0);
+    // count % 2=== 0 ? setmessage("I'm even") : setmessage("I'm odd");
+
+  }
+  const decrement=()=>{
+    setcount(count-1);
+    // setmessage(count % 2=== 0 ? "I'm even" : "I'm odd");    
+    // count % 2=== 0 ? setmessage("I'm even") : setmessage("I'm odd");
+  }
+
+  useEffect(()=>{
+    setmessage(count>0 && count%2===0?"I'm even":count===0?"Hii ":count<0 ?"I'm negative":"I'm odd")
+  },[count]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Count : {count}</h1>
+      <button onClick={increment} >Increment</button>
+      <button onClick={decrement} >Decrement</button>
+      <p>{message}</p>
+       
+    
+      {/* <p>{count % 2=== 0 ? message.setmessage("I'm even") : message.setmessage("I'm odd")}</p> */}
+
+
     </div>
   );
 }
